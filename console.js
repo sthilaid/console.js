801547f7b1087537d996fa36958b70d3cdf4a7e4
David St-Hilaire <sthilaire.david@gmail.com>
2019-10-25 21:07:43 -0400

    added evaluation, results, blink animation and more

--- console.js
+++ console.js
@@ -1,58 +1,115 @@
 
+const inputAnimDT = 1000
+const prompt = "> "
+const fontSize = 14
+const heightPadding = 2
+
 class Console
 {
-    constructor()
+    constructor(evalFn = null)
     {
         this.history = []
+        this.results = []
         this.cmd = ""
+        this.evalFn = evalFn
+        this.inputAnimLastTime = 0
+        this.inputAnimStr = ""
+    }
+
+    evalCmd() {
+        this.history.push(this.cmd)
+        if (this.evalFn) {
+            this.results.push(this.evalFn(this.cmd))
+        } else {
+            this.results.push([])
+        }
+        this.cmd = ""
+    }
+
+    refresh() {
+        if (!canvas || !context)
+            return
+
+        if (this.history.length != this.results.length) {
+            alert("history and results length should match ("
+                  +this.history.length+" != "+this.results.length+")")
+            return
+        }
+        
+        context.font = fontSize+"px Courier"
+        context.fillStyle = "black"
+        context.fillRect(0, 0, canvas.width, canvas.height)
+
+        context.fillStyle = "green"
+        var y = fontSize
+        context.fillText(" *** Welcome to super console 0.01b ***\n", 0, y)
+        y += fontSize*2
+
+        for(var i=0; i<this.history.length; ++i) {
+            context.fillText(prompt + this.history[i], 0, y)
+            y += fontSize + heightPadding
+            if (Array.isArray(this.results[i])) {
+                this.results[i].forEach(result => {
+                    context.fillText(r, 0, y)
+                    y += fontSize + heightPadding
+                })
+            } else if (this.results[i] != "") {
+                context.fillText(this.results[i], 0, y)
+                y += fontSize + heightPadding
+            }
+        }
+        
+        context.fillText(prompt + this.cmd +this.inputAnimStr, 0, y)
+        y += fontSize + heightPadding
+    }
+
+    step(time) {
+        let shouldChangeState = time - this.inputAnimLastTime > inputAnimDT
+        if (shouldChangeState) {
+            this.inputAnimStr = this.inputAnimStr == "" ? "_" : ""
+            this.inputAnimLastTime = time
+            this.refresh()
+            console.log("refreshing: "+this.inputAnimStr)
+        }
+        let obj = this
+        window.requestAnimationFrame(function(t){obj.step(t)})
     }
 }
 
-var consoleInstance = new Console()
-
 function onConsoleKey(event)
 {
-    //alert(event.key)
+    //console.log(event.key)
     if (event.key == "Alt" || event.key == "Control" || event.key == "Shift")
         return
     
     if (event.key == "Enter") {
-        if (consoleInstance.cmd != "") {
-            consoleInstance.history.push(consoleInstance.cmd)
-            consoleInstance.cmd = ""
-        }
+        consoleInstance.evalCmd()
     } else if (event.key == "Backspace") {
-        consoleInstance.cmd.substring(0, Math.max(consoleInstance.cmd.length-1, 0))
-    } else {
+        consoleInstance.cmd = consoleInstance.cmd.substring(0, Math.max(consoleInstance.cmd.length-1, 0))
+    } else if (event.key.length == 1){
         consoleInstance.cmd += event.key
     }
-    refresh()
+    consoleInstance.refresh()
 }
 
-function refresh() {
-    let fontSize = 12
-    let canvas = document.getElementById("ConsoleCanvas")
-    let context = canvas.getContext("2d")
-    context.font = fontSize+"px Courier"
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    
-    var y = fontSize
-    console.log("**** dumping: "+consoleInstance.history)
-    consoleInstance.history.forEach(h => {
-        context.fillText(h, 0, y)
-        y += fontSize + 2
-        console.log(h)
-    })
-    context.fillText("> " + consoleInstance.cmd +"_", 0, y)
-}
+var consoleInstance = null
+var canvas          = null
+var context         = null
 
 function main()
 {
-    let canvas = document.getElementById("ConsoleCanvas")
+    var x=0
+    consoleInstance = new Console(function(cmd){return "["+(x++)+"] "+cmd})
+    canvas = document.getElementById("ConsoleCanvas")
     if (canvas)
     {
-        canvas.tabIndex = 1000
+        context = canvas.getContext("2d")
+        
+        canvas.tabIndex = 1
+        canvas.focus()
         canvas.addEventListener("keydown", onConsoleKey)
+        consoleInstance.refresh()
+        window.requestAnimationFrame(function(step){consoleInstance.step(step)})
     }
 }
 
